ea0e5b472920aff5391de4f5eee7c4953913dc1c
Nikitorrr <nikgg1@mail.ru>
2025-11-01 12:33:51 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+function startExam() {
+    alert('Функция запуска экзамена будет добавлена! Сейчас идет разработка.');
+    // Здесь будет переход к билетам
+}
+
+// Базовая логика приложения
+console.log('ПДД Тренажер загружен!');
+
+// В будущем здесь будет подключение к Supabase
+const SUPABASE_URL = 'https://astcajoqsmklsrfbmxug.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzdGNham9xc21rbHNyZmJteHVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE4OTU2NTYsImV4cCI6MjA3NzQ3MTY1Nn0.SKPsvgFMrK0NN7Qwk_nlgHzrVBJlUleXFB-uzFdTGZw';
+
+// Функции для работы с билетами будут добавлены позже
